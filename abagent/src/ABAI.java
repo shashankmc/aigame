08286d5cc99f7a146efd7d0537a70e3d81153e9c
Shashank <shashankmc@github.com>
2021-10-14 19:22:48 +0200

    Fresh start

--- abagent/src/ABAI.java
+++ abagent/src/ABAI.java
@@ -2,48 +2,60 @@ import java.util.concurrent.ThreadLocalRandom;
 
 import game.Game;
 import main.collections.FastArrayList;
-import other.AI;
-import other.context.Context;
 import other.move.Move;
+import other.context.Context;
+import main.FileHandling;
+import main.collections.FVector;
+import other.state.State;
+import other.trial.Trial;
 import utils.AIUtils;
 
 
-public class ABAI extends AI{
+public class ABAI{
 	protected int player = -1;
+	
 	public static final float alpha_init = -100000.f;
-	public static final float beta_init = 100000.f;
-	
-	public ABAI() 
-	{
-		this.friendlyName = "Alpha beta trial";
-	}
-	
-	@Override
-	public Move selectAction
-	(
-			final Game game,
-			final Context context,
-			final double maxSeconds,
-			final int maxIterations,
-			final int maxDepth
-	)
-	{
-		FastArrayList<Move> legalMoves = game.moves(context).moves();
+	public static final float beta_init = -alpha_init;
+	boolean playerWon = false;
+	protected Context lastSearchedRootContext = null;
+	public ABAI() {
+		friendlyName = "Shashank's ABAI";
 		
-		if(!game.isAlternatingMoveGame())
-			legalMoves = AIUtils.extractMovesForMover(legalMoves, player);
-		
-		final int r = ThreadLocalRandom.current().nextInt(legalMoves.size());
-		return legalMoves.get(r);
 	}
-	
-	@Override
-	public void initAI(final Game game, final int playerID) 
+	public Move selectAction(final Game game, final Context context, final double maxSeconds, final int maxIterations, final int maxDepth)
 	{
-		this.player = playerID;
-	}
-	
+		playerWon = false;
+		final int depthLimit = maxDepth > 0 ? maxDepth : Integer.MAX_VALUE;
+		lastSearchedRootContext = context;
+		
+		final int initDepth = 2;
+		if (maxSeconds > 0)
+		{
+			final long startTime = System.currentTimeMillis();
+			final long stopTime = startTime + (long) (maxSeconds * 1000);
 			
+			final long currentTime = System.currentTimeMillis();
+			
+			//lastMove = alphaBetaSearch()
+			//return lastMove;
+		}
+		else {
+			// lastMove = alphaBetaSearch()
+			//return lastMove;
+		}
+		
+	}
+	public float alphaBeta(final Context context, final int depth, 
+			final float inAlpha, final float inBeta, final int maximisingPLayer, final long stopTime) {
+		final Trial trial = context.trial();
+		final State state = context.state();
+		
+		final float orgAlpha = inAlpha;
+		float alpha = inAlpha;
+		float beta = inBeta;
+		
+		fi
+	}		
 	
 }
 
